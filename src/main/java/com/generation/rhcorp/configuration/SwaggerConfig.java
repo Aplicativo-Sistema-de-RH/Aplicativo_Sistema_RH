1ee07c39233612531fb208687d1ffc4f8851c418
Bowie <larissa.soaress@yahoo.com.br>
2025-05-06 16:54:57 -0300

    Correção de dados na controller e config swagger

--- src/main/java/com/generation/rhcorp/configuration/SwaggerConfig.java
+++ src/main/java/com/generation/rhcorp/configuration/SwaggerConfig.java
@@ -15,41 +15,35 @@ import io.swagger.v3.oas.models.responses.ApiResponses;
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    OpenAPI springBlogPessoalOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Projeto Integrador - RH Corp")
-                        .description("Projeto Integrador - Projeto de Gerenciamento de RH feito pelo grupo 5")
-                        .version("v0.0.1")
-                        .license(new License()
-                                .name("JavaSon's Five")
-                                .url("https://github.com/Aplicativo-Sistema-de-RH/Aplicativo_Sistema_RH?tab=readme-ov-file#descri%C3%A7%C3%A3o-do-projeto"))
-                        .contact(new Contact()
-                                .name("JavaSon's Five")
-                                .url("https://github.com/Aplicativo-Sistema-de-RH/Aplicativo_Sistema_RH?tab=readme-ov-file#descri%C3%A7%C3%A3o-do-projeto")
-                                .email("grupo_5_generation@proton.me")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("JavaSon's Five")
-                        .url("https://github.com/Aplicativo-Sistema-de-RH/Aplicativo_Sistema_RH?tab=readme-ov-file#descri%C3%A7%C3%A3o-do-projeto"));
-    }
+	@Bean
+	OpenAPI springProjetoIntegradorOpenAPI() {
+		return new OpenAPI().info(new Info().title("Projeto Integrador - RH Corp")
+				.description("Projeto Integrador - Projeto de Gerenciamento de RH feito pelo grupo 5").version("v0.0.1")
+				.license(new License().name("JavaSon's Five").url(
+						"https://github.com/Aplicativo-Sistema-de-RH/Aplicativo_Sistema_RH?tab=readme-ov-file#descri%C3%A7%C3%A3o-do-projeto"))
+				.contact(new Contact().name("JavaSon's Five").url(
+						"https://github.com/Aplicativo-Sistema-de-RH/Aplicativo_Sistema_RH?tab=readme-ov-file#descri%C3%A7%C3%A3o-do-projeto")
+						.email("grupo_5_generation@proton.me")))
+				.externalDocs(new ExternalDocumentation().description("JavaSon's Five").url(
+						"https://github.com/Aplicativo-Sistema-de-RH/Aplicativo_Sistema_RH?tab=readme-ov-file#descri%C3%A7%C3%A3o-do-projeto"));
+	}
 
-    @Bean
-    OpenApiCustomizer customerGlobalHeadOpenApiCustomizer() {
+	@Bean
+	OpenApiCustomizer customerGlobalHeadOpenApiCustomizer() {
 
-        return openApi -> {
-            openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
-                ApiResponses apiResponses = operation.getResponses();
-                apiResponses.addApiResponse("200", new ApiResponse().description("Sucesso!"));
-                apiResponses.addApiResponse("201", new ApiResponse().description("Criado!"));
-                apiResponses.addApiResponse("204", new ApiResponse().description("Sem Conteudo!"));
-                apiResponses.addApiResponse("400", new ApiResponse().description("Erro na Requisição!"));
-                apiResponses.addApiResponse("401", new ApiResponse().description("Não Autorizado!"));
-                apiResponses.addApiResponse("403", new ApiResponse().description("Proibido!"));
-                apiResponses.addApiResponse("404", new ApiResponse().description("Não Encontrado!"));
-                apiResponses.addApiResponse("500", new ApiResponse().description("Erro na Aplicação!"));
-            }));
-        };
-    }
+		return openApi -> {
+			openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+				ApiResponses apiResponses = operation.getResponses();
+				apiResponses.addApiResponse("200", new ApiResponse().description("Sucesso!"));
+				apiResponses.addApiResponse("201", new ApiResponse().description("Criado!"));
+				apiResponses.addApiResponse("204", new ApiResponse().description("Sem Conteudo!"));
+				apiResponses.addApiResponse("400", new ApiResponse().description("Erro na Requisição!"));
+				apiResponses.addApiResponse("401", new ApiResponse().description("Não Autorizado!"));
+				apiResponses.addApiResponse("403", new ApiResponse().description("Proibido!"));
+				apiResponses.addApiResponse("404", new ApiResponse().description("Não Encontrado!"));
+				apiResponses.addApiResponse("500", new ApiResponse().description("Erro na Aplicação!"));
+			}));
+		};
+	}
 
 }
